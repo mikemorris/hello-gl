b87312138cee6b7cae100a4922811f4ae6f382db
Mike Morris <michael.patrick.morris@gmail.com>
2014-02-25 12:58:58 -0500

    define webgl buffers and antialiasing

--- js/index.js
+++ js/index.js
@@ -4,7 +4,12 @@ var shaders = require('../dist/shaders');
 var canvas = document.getElementById('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-var gl = canvas.getContext('webgl');
+var gl = canvas.getContext('webgl', {
+  antialias: false,
+  alpha: true,
+  stencil: true,
+  depth: false
+});
 
 var vertexShader = gl.createShader(gl.VERTEX_SHADER);
 gl.shaderSource(vertexShader, shaders.shader.vertex);
@@ -27,10 +32,10 @@ var vertexBuffer = gl.createBuffer();
 gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 
 gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-  1.0,  1.0,
-  -1.0,  1.0,
-  1.0, -1.0,
-  -1.0, -1.0
+  0.5,  0.5,
+  -0.5,  0.5,
+  0.5, -0.5,
+  -0.5, -0.5
 ]), gl.STATIC_DRAW);
 
 gl.vertexAttribPointer(vertexPositionAttribute, 2, gl.FLOAT, false, 0, 0);
